4d1212af799b46f8ebc9de923a465ab190f6150f
Jacob Albright <jacob.albright23@gmail.com>
2019-09-29 21:27:08 -0500

    styled login with google button

---
+++ components/login/google-login-btn.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-hover v-model="hover">
+    <button @click="$auth.login('google')">
+      <v-img contain :src="src" width="220"></v-img>
+    </button>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hover: false
+    }
+  },
+  computed: {
+    src() {
+      const variant = this.hover ? 'pressed' : 'normal';
+      return require(`~/assets/img/btn_google_signin_light_${variant}_web@2x.png`)
+    }
+  }
+}
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,47 @@
 <template>
-  <v-layout>
-    <v-flex>
-      <v-btn @click="$auth.login('google')">Sign in with Google</v-btn>
+  <v-layout justify-center align-center>
+    <v-flex xs4>
+      <v-card>
+        <v-card-title class="title text-xs-center">
+          Log in or create an account
+        </v-card-title>
+        <v-card-text>
+
+          <template v-for="(b, i) in buttons">
+            <v-layout :key="i" justify-center>
+              <GoogleLoginButton />
+            </v-layout>
+          </template>
+
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import GoogleLoginButton from '@/components/login/google-login-btn'
 export default {
-  auth: false
+  auth: false,
+  components: {
+    GoogleLoginButton,
+  },
+  computed: {
+    buttons() {
+      const srcs = {
+        normal: require('~/assets/img/btn_google_signin_light_normal_web@2x.png'),
+        focus: require('~/assets/img/btn_google_signin_light_focus_web@2x.png'),
+        pressed: require('~/assets/img/btn_google_signin_light_pressed_web@2x.png'),
+      }
+      return [
+        {
+          class: 'mb-3',
+          src: srcs.normal,
+          // /Users/jacobalbright/development/answerth3question/frontend/assets/img/btn_google_signin_light_pressed_web@2x.png
+          onClick: () => this.$auth.login('google'),
+        }
+      ]
+    }
+  }
 }
 </script>
